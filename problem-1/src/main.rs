191222e951c5ee9be55a3b8352836d3d0325537e
Hellevar <Hellevar@gmail.com>
2024-12-30 08:40:10 +0300

    Rework with hashmap

--- problem-1/src/main.rs
+++ problem-1/src/main.rs
@@ -1,16 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
 }
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut nums_hashmap: HashMap<i32, usize> = HashMap::new();
     let mut result: Vec<i32> = vec![];
     for (index, num) in nums.iter().enumerate() {
-        for (inner_index, inner_num) in nums.iter().enumerate() {
-            if index != inner_index {
-                if num + inner_num == target {
-                    result = vec![inner_index as i32, index as i32];
-                }
-            }
+        let difference = target - num;
+        if let Some(another_index) = nums_hashmap.get(&difference) {
+            result = vec![index as i32, *another_index as i32];
+        } else {
+            nums_hashmap.insert(*num, index);
         }
     }
 
